8af75e411f03dfed49adb71331491aea0b2bfe58
estherokondev <estherakpanokon@gmail.com>
2025-05-17 12:02:39 +0100

    Functions.cs
    FUNCTIONS

---
+++ Functions.cs
@@ -0,0 +1,47 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+
+    // Function 1
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Function 2
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    // Function 3
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string input = Console.ReadLine();
+        return int.Parse(input);
+    }
+
+    // Function 4
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    // Function 5
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {s
